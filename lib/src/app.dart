2c42c80a115034699b93b6c4766c0d922ae97a84
Daniel Schischkin <daniel@schischkin.info>
2021-09-27 00:40:54 +0200

    Fix linting problem in app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,7 +5,7 @@ import "package:flutter_localizations/flutter_localizations.dart";
 /// Start Point
 class HabboFurniture extends StatelessWidget {
   // ignore: public_member_api_docs
-  const HabboFurniture({Key? key,}) : super(key: key);
+  const HabboFurniture({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +21,8 @@ class HabboFurniture extends StatelessWidget {
         Locale("en", ""),
         Locale("de", ""),
       ],
-      onGenerateTitle: (BuildContext context) => AppLocalizations.of(context)!.appTitle,
+      onGenerateTitle: (BuildContext context) =>
+        AppLocalizations.of(context)!.appTitle,
       theme: ThemeData(),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
