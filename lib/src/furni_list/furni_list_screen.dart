a47b1040c1641ee82f89469e1da5b064c5e1da05
Daniel Schischkin <daniel@schischkin.info>
2021-10-01 20:24:56 +0200

    Update navigation drawer by adding a profile picture

--- lib/src/furni_list/furni_list_screen.dart
+++ lib/src/furni_list/furni_list_screen.dart
@@ -1,67 +1,9 @@
 import "package:flutter/material.dart";
+import "package:habbo_furniture/src/navigation/navigation.dart";
 
 class FurniListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Image.asset("images/logo.png", fit: BoxFit.contain),
-        toolbarHeight: 100,
-      ),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            const DrawerHeader(
-              child: Center(
-                child: Text("lapsed", style: TextStyle(color: Colors.white))
-              ),
-              decoration:  BoxDecoration(
-                color: Colors.blue
-              ),
-            ),
-            Expanded(child: ListView(
-              padding: EdgeInsets.zero,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                      labelText: "Search",
-                      border: OutlineInputBorder()
-                    ),
-                  )
-                ),
-                ListTile(
-                  leading: const Icon(Icons.home),
-                  title: const Text("Home"),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(Icons.favorite),
-                  title: const Text("Favorites"),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ]
-            )),
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: ListTile(
-                  leading: const Icon(Icons.logout),
-                  title: const Text("Logout"),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              )
-            ),
-          ],
-        ),
-      ),
-    );
+    return Navigation(const Text("hi"));
   }
 }
